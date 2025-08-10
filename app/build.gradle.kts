39cbafef66a10e2de05433f041c6c6ebf9ea8f86
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-08-10 23:38:04 +0700

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
@@ -9,11 +9,8 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "mvvmi"
         minSdk = 27
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
